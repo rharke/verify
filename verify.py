6a3b111ae25b157cdf71f01833ff8f8159d86032
Ranger Harke <ranger.harke@gmail.com>
2014-03-16 21:40:59 -0400

    More refactoring

--- verify.py
+++ verify.py
@@ -46,6 +46,11 @@ class Verifier(object):
         self.args = args
         self.database = {}
 
+        self.failed = 0
+        self.verified = 0
+        self.added = 0
+        self.removed = 0
+
     def read_database(self):
         if os.path.exists(self.args.database_file):
             with open(self.args.database_file, 'r+a') as sumfile:
@@ -60,70 +65,68 @@ class Verifier(object):
             for filepath in self.database:
                 sumfile.write('%s  %s\n' % (self.database[filepath][0], filepath))
 
+    def check_local_file(self, filepath):
+        if filepath in self.database:
+            self.log('Existing file %s... ' % (filepath,))
+            if self.args.verify_existing:
+                checksum = md5sum(filepath)
+                if checksum != self.database[filepath][0]:
+                    if self.args.update_changed:
+                        self.log('updated\n')
+                        self.database[filepath][0] = checksum
+                    else:
+                        self.log('failed\n')
+                    self.failed += 1
+                else:
+                    self.log('verified\n')
+                    self.verified += 1
+            else:
+                self.log('skipped\n')
+            self.database[filepath][1] = True
+        else:
+            self.log('New file %s... ' % (filepath,))
+            if self.args.add_new:
+                checksum = md5sum(filepath)
+                self.log('computed\n')
+                self.database[filepath] = [checksum, True]
+            else:
+                self.log('skipped\n')
+            self.added += 1
+
+    def check_database_file(self, filepath):
+        if not self.database[filepath][1]:
+            self.log('Deleted file %s... ' % (filepath,))
+            if self.args.remove_deleted:
+                del self.database[filepath]
+                self.log('removed\n')
+            else:
+                self.log('skipped\n')
+            self.removed += 1
+
     def run(self):
         self.read_database()
-        
-        failed = 0
-        verified = 0
-        added = 0
-        removed = 0
 
         for dirpath, dirnames, filenames in os.walk(self.args.verify_directory):
             for filename in filenames:
-                filepath =  os.path.join(dirpath, filename)
-                if filepath in self.database:
-                    self.log('Existing file %s... ' % (filepath,))
-                    if self.args.verify_existing:
-                        checksum = md5sum(filepath)
-                        if checksum != self.database[filepath][0]:
-                            if self.args.update_changed:
-                                self.log('updated\n')
-                                self.database[filepath][0] = checksum
-                            else:
-                                self.log('failed\n')
-                            failed += 1
-                        else:
-                            self.log('verified\n')
-                            verified += 1
-                    else:
-                        self.log('skipped\n')
-                    self.database[filepath][1] = True
-                else:
-                    self.log('New file %s... ' % (filepath,))
-                    if self.args.add_new:
-                        checksum = md5sum(filepath)
-                        self.log('computed\n')
-                        self.database[filepath] = [checksum, True]
-                    else:
-                        self.log('skipped\n')
-                    added += 1
+                filepath = os.path.join(dirpath, filename)
+                self.check_local_file(filepath)
 
-        todelete = []  # do not delete immediately because we are iterating
-        for filepath in self.database:
-            if not self.database[filepath][1]:
-                self.log('Deleted file %s... ' % (filepath,))
-                if self.args.remove_deleted:
-                    todelete.append(filepath)
-                    self.log('removed\n')
-                else:
-                    self.log('skipped\n')
-                removed += 1
+        # NB: iterate over keys so we can delete while iterating
+        for filepath in self.database.keys():
+            self.check_database_file(filepath)
 
-        for filepath in todelete:
-            del self.database[filepath]
-
-        fail_update = ((failed > 0) and self.args.update_changed)
-        add_update = ((added > 0) and self.args.add_new)
-        del_update = ((removed > 0) and self.args.remove_deleted)
+        fail_update = (self.failed > 0) and self.args.update_changed
+        add_update = (self.added > 0) and self.args.add_new
+        del_update = (self.removed > 0) and self.args.remove_deleted
 
         if fail_update or add_update or del_update:
             self.write_database()
 
         self.log('\nSummary:\n')
-        self.log('    %d verified\n' % (verified,))
-        self.log('    %d failed%s\n' % (failed, ' (database updated)' if fail_update else ''))
-        self.log('    %d new files%s\n' % (added, ' (database updated)' if add_update else ''))
-        self.log('    %d deleted files%s\n' % (removed, ' (database updated)' if del_update else ''))
+        self.log('    %d verified\n' % (self.verified,))
+        self.log('    %d failed%s\n' % (self.failed, ' (database updated)' if fail_update else ''))
+        self.log('    %d new files%s\n' % (self.added, ' (database updated)' if add_update else ''))
+        self.log('    %d deleted files%s\n' % (self.removed, ' (database updated)' if del_update else ''))
 
     def log(self, message):
         if self.args.verbose:
